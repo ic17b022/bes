a6cce131edbc1f0977637ad6e2573a3dd7340566
ic17b022 <ic17b022@technikum-wien.at>
2018-03-03 10:40:43 +0100

    removed argument handling

--- myFind.c
+++ myFind.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int main(int argc, char* argv[]) {
+    int iRc = 0;
+
+    if(argc > 0)
+    {
+        printf("Ivalid arguments\nCorrect usage: No arguments");
+        iRc = 1;
+    }
+
+    return iRc;
 }
